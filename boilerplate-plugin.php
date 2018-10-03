83ad6ede951b7951f5f32ba6a68f18ceee6b8780
Richard Pastenes <richard@pastenes.com>
2018-10-02 21:33:21 -0700

    Initial commit

---
+++ boilerplate-plugin.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Plugin Name:       Boilerplate Plugin
+Description:       Basic boilerplate starter plugin
+Plugin URI:        https://richard.pastenes.com
+Contributors:      (list of wordpress.org usernames)
+Author:            Richard Pastenes
+Author URI:        https://richard.pastenes.com
+Donate link:       https://richard.pastenes.com
+Tags:              login, boilerplate
+Version:           1.0
+Stable tag:        1.0
+Requires at least: 4.5
+Tested up to:      4.8
+Text Domain:       boilerplate-plugin
+Domain Path:       /languages
+License:           GPL v2 or later
+License URI:       https://www.gnu.org/licenses/gpl-2.0.txt
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version
+2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+with this program. If not, visit: https://www.gnu.org/licenses/
+*/
+
+// Exit if file is called directly
+if (!defined('ABSPATH')) {
+    exit;
+}
